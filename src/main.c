4458ea8aa872108b2614e160513371c1d24946cc
Aleix Kambou <akambou@car8s2.42barcelona.com>
2024-04-10 01:30:50 +0200

    si

---
+++ src/main.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akambou <akambou@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/06 09:43:14 by kmb               #+#    #+#             */
+/*   Updated: 2024/04/10 01:23:18 by akambou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	print_header(void)
+{
+	char	*header;
+
+	header = "\033[1;31m" \
+	"███╗   ███╗██╗███╗   ██╗██╗\n" \
+	"████╗ ████║██║████╗  ██║██║\n" \
+	"██╔████╔██║██║██╔██╗ ██║██║\n" \
+	"██║╚██╔╝██║██║██║╚██╗██║██║\n" \
+	"██║ ╚═╝ ██║██║██║ ╚████║██║\n" \
+	"╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝\n" \
+	"\n" \
+	"███╗   ███╗ █████╗ ██╗   ██╗ █████╗ ███╗   ██╗██████╗███████╗ ██████╗\n" \
+	"████╗ ████║██╔══██╗██║   ██║██╔══██╗████╗  ██║╚═██╔═╝██╔════╝██╔═══██╗\n" \
+	"██╔████╔██║███████║██║   ██║███████║██╔██╗ ██║  ██║  █████╗  ██║   ██║\n" \
+	"██║╚██╔╝██║██╔══██║██║   ██║██╔══██║██║╚██╗██║  ██║  ██╔══╝  ██║   ██║\n" \
+	"██║ ╚═╝ ██║██║  ██║█████╗██║██║  ██║██║ ╚████║  ██║  ███████╗╚██████╔╝\n" \
+	"╚═╝     ╚═╝╚═╝  ╚═╝╚════╝╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝  ╚═╝  ╚══════╝ ╚═════╝\n" \
+	"\n" \
+	"& CACHETEO SHELL.\n" \
+	"\033[0m";
+	ft_printf("\n%s\n", header);
+}
+
+char	*create_prompt(void)
+{
+	char	*username;
+	char	*prompt;
+
+	username = getenv("USER");
+	if (username == NULL)
+		username = "unknown";
+	prompt = malloc(ft_strlen(username) + ft_strlen("@minimalianteo$ ") + 1);
+	ft_strlcpy(prompt, username, ft_strlen(username)+ 1);
+	ft_strlcat(prompt, "@minimalianteo$ ", ft_strlen(username) \
+	+ ft_strlen("@minimalianteo$ ") + 1);
+	return (prompt);
+}
+
+void	handle_sigint(int sig)
+{
+	if (sig == SIGINT)
+	{
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		printf("\n");
+		char *new_prompt = create_prompt();
+		rl_set_prompt(new_prompt);
+		free(new_prompt);
+		rl_redisplay();
+	}
+}
+
+int	main(void)
+{
+	char				*input;
+	char				*prompt;
+	int					status;
+	t_commandhistory	*history;
+
+	signal(SIGINT, handle_sigint);
+	history = create_history();
+	print_header();
+	status = 0;
+	while (1)
+	{
+		prompt = create_prompt();
+		input = readline(prompt);
+		free(prompt);
+		if (input && *input)
+			add_history(input);
+		if (input == NULL)
+			exit(EXIT_FAILURE);
+		if (ft_strncmp(input, "echo $?", 8) == 0)
+			ft_printf("minimalianteo: %d\n", status);
+		status = parse_command(input, history);
+		free(input);
+	}
+	return (0);
+}
---
+++ src/parsing/parsing.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akambou <akambou@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/06 16:58:05 by kmb               #+#    #+#             */
+/*   Updated: 2024/04/10 01:27:48 by akambou          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	handle_commands(t_command_data *command)
+{
+	int j = 0;
+	while (command->commands[command->i][j] != '\0')
+	{
+		if (command->commands[command->i][j] == '$'
+			&& command->commands[command->i][j + 1] != '?'
+			&& (is_single_quote(command->commands[command->i], \
+			j) == 0))
+			handle_variable_expansion(command->commands, &j, \
+				command->is_malloced, command->var_name);
+		else
+			j++;
+	}
+}
+
+int	chose_command(t_command_data *command, t_commandhistory *history)
+{
+	t_pipe_data	data;
+
+	command->i -= 1;
+	if (command->i < 0)
+		return (0);
+	else
+	{
+		initialize_variables(&data.i, &data.fd_in);
+		data.status = chose_pipe(command, &data, history);
+		return (data.status);
+	}
+	return (data.status);
+}
+
+int	parse_command(char *input, t_commandhistory *history)
+{
+	t_command_data		command;
+	int					status;
+
+	initialize_command_data(&command);
+	add__history(history, input);
+	if (handle_exception(input) == 0)
+		return (0);
+	command.commands[command.i] = ft_strtok(input, "|");
+	while (command.commands[command.i] != NULL)
+	{
+		handle_commands(&command);
+		command.i++;
+		command.commands[command.i] = ft_strtok(NULL, "|");
+	}
+	status = chose_command(&command, history);
+	return (status);
+}
